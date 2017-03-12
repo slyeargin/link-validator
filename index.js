4727cd74bdbfa26255b0eab5945c5387abe88093
Samantha Yeargin <slyeargin@gmail.com>
2017-03-12 14:43:34 -0500

    index.validSyntax determines if a URL is valid or not.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var validUrl = require('valid-url');
+
 // var links = process.argv.slice(2);
 // validateUrls(links);
 
@@ -10,12 +12,21 @@ function objectify(link) {
   return linkObject;
 }
 
-// function validSyntax(link)
+function validSyntax(link) {
+  if (validUrl.isWebUri(link.url)) {
+    link.validSyntax = true;
+    return link;
+  } else {
+    link.validSyntax = false;
+    return link;
+  }
+}
 
 // function validResponse(link)
 
 // function printAnalysis(link)
 
 module.exports = {
-  objectify: objectify
+  objectify: objectify,
+  validSyntax: validSyntax
 }
--- test/index_test.js
+++ test/index_test.js
@@ -11,3 +11,27 @@ describe('.objectify', function() {
     done();
   });
 });
+
+describe('.validSyntax', function() {
+  it('identifies a syntactically-valid URL', function(done){
+    var link = {};
+    link.url = "http://example.com";
+    var result = index.validSyntax(link);
+    expect(result).to.be.ok;
+    expect(result).to.be.an.object;
+    expect(result.validSyntax).to.be.true;
+    expect(result.url).to.equal(link.url);
+    done();
+  });
+
+  it('identifies a syntactically-invalid URL', function(done){
+    var link = {};
+    link.url = "foo://example.net";
+    var result = index.validSyntax(link);
+    expect(result).to.be.ok;
+    expect(result).to.be.an.object;
+    expect(result.validSyntax).to.be.false;
+    expect(result.url).to.equal(link.url);
+    done();
+  });
+});
